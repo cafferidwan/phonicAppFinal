64cbf55903a5a6c12b2726cb004c8a33cd19b5e9
cafferidwan <quazi.hasib13@gmail.com>
2013-09-25 16:47:35 +0600

    1st commit

---
+++ src/com/example/phonicsapp/MainActivity.java
@@ -0,0 +1,183 @@
+package com.example.phonicsapp;
+
+import org.andengine.engine.camera.Camera;
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.andengine.entity.scene.Scene;
+import org.andengine.entity.scene.background.Background;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.input.touch.TouchEvent;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.andengine.opengl.texture.atlas.bitmap.BuildableBitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.source.IBitmapTextureAtlasSource;
+import org.andengine.opengl.texture.atlas.buildable.builder.BlackPawnTextureAtlasBuilder;
+import org.andengine.opengl.texture.atlas.buildable.builder.ITextureAtlasBuilder.TextureAtlasBuilderException;
+import org.andengine.opengl.texture.region.ITextureRegion;
+import org.andengine.ui.activity.SimpleBaseGameActivity;
+import org.andengine.util.color.Color;
+import org.andengine.util.debug.Debug;
+import android.content.Context;
+import android.view.Display;
+
+public class MainActivity extends SimpleBaseGameActivity
+{
+
+	static int CAMERA_WIDTH;
+	static int CAMERA_HEIGHT;
+
+	public Camera mCamera;
+	public static Scene mScene;
+	static Context context;
+	
+	private BuildableBitmapTextureAtlas mBitmapTextureAtlas;
+	public static ITextureRegion mKolomTextureRegion;
+	public static ITextureRegion mBoardTextureRegion;
+	public static ITextureRegion mParrotTextureRegion;
+	
+	public static ITextureRegion mbackGroundTextureRegion;
+	
+	public static Sprite backGround, parrot, board, kolom;
+
+	
+	@Override
+	public EngineOptions onCreateEngineOptions()
+	{
+		// TODO Auto-generated method stub
+		Display display = getWindowManager().getDefaultDisplay();
+		CAMERA_HEIGHT = display.getHeight();
+		CAMERA_WIDTH = display.getWidth();
+		
+		mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+		
+		return new EngineOptions(true, ScreenOrientation.LANDSCAPE_SENSOR,new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), mCamera);
+	
+	}
+
+	@Override
+	protected void onCreateResources() 
+	{
+		// TODO Auto-generated method stub
+		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
+
+		this.mBitmapTextureAtlas = new BuildableBitmapTextureAtlas(
+				this.getTextureManager(), 1600, 1200);
+
+		MainActivity.mbackGroundTextureRegion = BitmapTextureAtlasTextureRegionFactory
+				.createFromAsset(this.mBitmapTextureAtlas, this, "bg3.png");
+		MainActivity.mKolomTextureRegion = BitmapTextureAtlasTextureRegionFactory
+				.createFromAsset(this.mBitmapTextureAtlas, this, "kolom.png");
+		MainActivity.mBoardTextureRegion = BitmapTextureAtlasTextureRegionFactory
+				.createFromAsset(this.mBitmapTextureAtlas, this, "kolom.png");
+		MainActivity.mParrotTextureRegion = BitmapTextureAtlasTextureRegionFactory
+				.createFromAsset(this.mBitmapTextureAtlas, this, "kolom.png");
+		
+		try 
+		{
+			this.mBitmapTextureAtlas.build(new BlackPawnTextureAtlasBuilder<IBitmapTextureAtlasSource,
+					BitmapTextureAtlas>(0, 0, 0));
+			this.mBitmapTextureAtlas.load();
+		} 
+		
+		catch (TextureAtlasBuilderException e)
+		{
+			Debug.e(e);
+		}
+		
+
+	}
+
+	@Override
+	protected Scene onCreateScene()
+	{
+		// TODO Auto-generated method stub
+		mScene = new Scene();
+		mScene.setBackground(new Background(Color.WHITE));
+		mScene.setTouchAreaBindingOnActionDownEnabled(true);
+		
+		backGround = new Sprite(0, 0, mbackGroundTextureRegion, getVertexBufferObjectManager());
+		backGround.setHeight(CAMERA_HEIGHT);
+		backGround.setWidth(CAMERA_WIDTH);
+		mScene.attachChild(backGround);
+		
+		parrot = new Sprite(CAMERA_WIDTH/3-300, CAMERA_HEIGHT-300, mParrotTextureRegion, getVertexBufferObjectManager())
+		{
+			@Override
+			public boolean onAreaTouched(final TouchEvent pSceneTouchEvent, final float pTouchAreaLocalX, final float pTouchAreaLocalY)
+			{
+				switch (pSceneTouchEvent.getAction()) 
+				{
+					case TouchEvent.ACTION_DOWN:
+					
+						this.setScale((float) 1.5);
+					break;
+					case TouchEvent.ACTION_UP:
+						
+						this.setScale((float) 1.0);
+					break;
+				}
+
+				return true;
+			}
+		};
+		parrot.setHeight(CAMERA_HEIGHT/3);
+		parrot.setWidth(CAMERA_WIDTH/3);
+		mScene.registerTouchArea(parrot);
+		mScene.attachChild(parrot);
+		
+		kolom = new Sprite(CAMERA_WIDTH/2-100, CAMERA_HEIGHT-300, mKolomTextureRegion, getVertexBufferObjectManager())
+		{
+			@Override
+			public boolean onAreaTouched(final TouchEvent pSceneTouchEvent, final float pTouchAreaLocalX, final float pTouchAreaLocalY)
+			{
+				switch (pSceneTouchEvent.getAction()) 
+				{
+				case TouchEvent.ACTION_DOWN:
+					
+					this.setScale((float) 1.5);
+				break;
+				case TouchEvent.ACTION_UP:
+					
+					this.setScale((float) 1.0);
+				break;
+				}
+
+				return true;
+			}
+		};
+		kolom.setHeight(CAMERA_HEIGHT/3);
+		kolom.setWidth(CAMERA_WIDTH/3);
+		mScene.registerTouchArea(kolom);
+		mScene.attachChild(kolom);
+
+		board = new Sprite(CAMERA_WIDTH/2+100, CAMERA_HEIGHT-300, mKolomTextureRegion, getVertexBufferObjectManager())
+		{
+			@Override
+			public boolean onAreaTouched(final TouchEvent pSceneTouchEvent, final float pTouchAreaLocalX, final float pTouchAreaLocalY)
+			{
+				switch (pSceneTouchEvent.getAction()) 
+				{
+				case TouchEvent.ACTION_DOWN:
+					
+					this.setScale((float) 1.5);
+				break;
+				case TouchEvent.ACTION_UP:
+					
+					this.setScale((float) 1.0);
+				break;
+				}
+
+				return true;
+			}
+		};
+		board.setHeight(CAMERA_HEIGHT/3);
+		board.setWidth(CAMERA_WIDTH/3);
+		mScene.registerTouchArea(board);
+		mScene.attachChild(board);
+		
+		return mScene;
+	}
+
+
+}
